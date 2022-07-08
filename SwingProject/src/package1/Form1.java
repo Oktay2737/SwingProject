6a04c88732e0b074b8a308fdf14d20c0bb3879c6
Oktay2737 <ozvb2737@gmail.com>
2022-07-08 20:35:41 +0200

    comboBox

--- SwingProject/src/package1/Form1.java
+++ SwingProject/src/package1/Form1.java
@@ -1,15 +1,27 @@
 package package1;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Form1 extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel Window;
+	
+	DefaultComboBoxModel model_red = new DefaultComboBoxModel<>();
+	DefaultComboBoxModel model_green = new DefaultComboBoxModel<>();
+	DefaultComboBoxModel model_blue = new DefaultComboBoxModel<>();
+	int r,g,b;
 
 	/**
 	 * Launch the application.
@@ -31,12 +43,70 @@ public class Form1 extends JFrame {
 	 * Create the frame.
 	 */
 	public Form1() {
+		r=255;g=255;b=255;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		Window = new JPanel();
+		Window.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(Window);
+		Window.setLayout(null);
+		
+		JComboBox RedValue = new JComboBox();
+		RedValue.setBounds(31, 34, 74, 22);
+		Window.add(RedValue);
+		
+		JComboBox GreenValue = new JComboBox();
+		GreenValue.setBounds(165, 34, 74, 22);
+		Window.add(GreenValue);
+		
+		JComboBox BlueValue = new JComboBox();
+		BlueValue.setBounds(297, 34, 81, 22);
+		Window.add(BlueValue);
+		
+		for(int i =0; i<256; i++) {
+			model_red.addElement(i);
+			model_green.addElement(i);
+			model_blue.addElement(i);
+		}
+		RedValue.setModel(model_red);
+		GreenValue.setModel(model_green);
+		BlueValue.setModel(model_blue);
+		
+		JLabel redBoard = new JLabel("Red");
+		redBoard.setOpaque(true);
+		redBoard.setHorizontalAlignment(SwingConstants.CENTER);
+		redBoard.setBounds(31, 78, 46, 14);
+		Window.add(redBoard);
+		
+		JLabel greenBoard = new JLabel("Green");
+		greenBoard.setOpaque(true);
+		greenBoard.setHorizontalAlignment(SwingConstants.CENTER);
+		greenBoard.setBounds(165, 78, 46, 14);
+		Window.add(greenBoard);
+		
+		JLabel blueBoard = new JLabel("Blue");
+		blueBoard.setOpaque(true);
+		blueBoard.setHorizontalAlignment(SwingConstants.CENTER);
+		blueBoard.setBounds(297, 78, 46, 14);
+		Window.add(blueBoard);
+		
+		RedValue.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				r=RedValue.getSelectedIndex();
+				redBoard.setBackground(new Color(r,0,0));
+			}
+		});
+		GreenValue.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				g=RedValue.getSelectedIndex();
+				greenBoard.setBackground(new Color(0,g,0));
+			}
+		});
+		BlueValue.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				b=BlueValue.getSelectedIndex();
+				blueBoard.setBackground(new Color(0,0,b));
+			}
+		});
 	}
-
 }
